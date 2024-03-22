cdf509a6d6630e2b19fd2b34b866cad115c4456c
BerlyCS <berly.mandamientos@gmail.com>
2024-03-21 22:24:57 -0500

    Demo del programa con algoritmo burbuja

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
+#include <cstdlib>
 #include <ctime>
 #include <iostream>
+#include <ostream>
 #include <string>
 #include <vector>
 
@@ -12,6 +14,8 @@ using namespace std;
 #define BLUE    "\033[34m"
 #define MAGENTA "\033[35m"
 #define CYAN    "\033[36m"
+
+float sleep;
 
 void create_grid(string **&grid, int num, vector<int> array){
     grid = new string*[num];
@@ -32,15 +36,30 @@ void create_grid(string **&grid, int num, vector<int> array){
     }
 }
 
-void print_grid(string**& grid, int num) {
+void swap_grid(string**& grid, int num, int lt, int gt) {
+    for (int i=0;i<num;i++) {
+        swap(grid[i][lt],grid[i][gt]);
+    }
+}
+
+void print_grid(string**& grid, int num, int curr, int mv) {
     system("clear");
+
     for (int i=0;i<num;i++) {
         for (int j=0;j<num;j++){
-            cout<<grid[i][j];
+            if (j==curr)
+                cout<<RED<<grid[i][j]<<RESET<<flush;
+            else if (j==mv)
+                cout<<GREEN<<grid[i][j]<<RESET<<flush;
+            else
+                cout<<grid[i][j]<<flush;
         }
         cout<<'\n';
     }
+    cout<<'\n';
+    system("sleep 0.2");
 }
+
 
 void shuffle_vector(vector<int> &array){
     int size=array.size();
@@ -53,12 +72,28 @@ void shuffle_vector(vector<int> &array){
     }
 }
 
+void bubble_sort(vector<int> &array, int num, string **&grid) {
+    for (int i=num-1;i>=0;i--) {
+        for (int j=0;j<i;j++) {
+            if (array[j] > array[j+1]) {
+                swap(array[j], array[j+1]);
+                swap_grid(grid, num, j, j+1);
+            }
+            print_grid(grid, num,j,j+1);
+        }
+    }
+}
+
 int main(int argc, char* argv[]) {
     if (argc < 2) {
-        cout<<"Uso: "<<argv[0]<<" [Cantidad]\n";
+        cout<<"Uso: "<<argv[0]<<" [Cantidad] [sleep]\n";
         return 1;
     }
     int num=stoi(argv[1]);
+    if (argc==3)
+        sleep=stof(argv[2]);
+    else
+        sleep=0.5;
 
     vector<int> array(num);
     shuffle_vector(array);
@@ -66,6 +101,9 @@ int main(int argc, char* argv[]) {
     bool sorted=false;
     string **grid;
     create_grid(grid, num, array);
-    print_grid(grid, num);
-
+    /* print_grid(grid, num); */
+    bubble_sort(array, num, grid);
+    
+    for (int i=0;i<num;i++)
+        cout<<array[i]<<' ';
 }
