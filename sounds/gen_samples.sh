3a051e84097ef4ed61f596eae0c4c526f518c6b4
BerlyCS <berly.mandamientos@gmail.com>
2024-03-25 11:10:51 -0500

    Sonidos

--- sounds/gen_samples.sh
+++ sounds/gen_samples.sh
@@ -4,5 +4,7 @@ set -e
 
 for (( i=0;i<10;i++ ))
 do
-    sox -n -r 44100 -c 2 "beep$i.wav" synth 0.1 sine $((i*100+500))
+    sox -n -r 22100 -c 1 "beep$i.wav" synth 0.1 sine $((i*150+400))
 done
+
+
