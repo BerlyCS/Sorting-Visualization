2575be5d9ad5a8870616dda36e7faa63056970bb
BerlyCS <berly.mandamientos@gmail.com>
2024-06-07 17:53:20 -0500

    Fix: script de generacion de sonidos

--- sounds/gen_samples.sh
+++ sounds/gen_samples.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-MAX=3000
+MAX=5000
 STEP=$((($MAX-300)/$1))
 
 for (( i = 0;i < $1; i++ ))
 do
-    sox -n -r 44100 -c 1 "$i.wav" synth 0.1 sine $((i*150+300))
+    sox -n -r 44100 -c 1 "$i.wav" synth 0.1 sine $((i*STEP+300))
 done
